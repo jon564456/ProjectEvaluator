b0c73d7039a4f563a9d7dfa2b08b94d62c13cabe
BR1G1D0 <jon5644568@gmail.com>
2023-09-17 23:25:16 -0600

    Primera Version

---
+++ app/src/main/java/com/itsoeh/jbrigido/projectevaluator/RecoveryActivity.java
@@ -0,0 +1,14 @@
+package com.itsoeh.jbrigido.projectevaluator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recovery);
+    }
+}
