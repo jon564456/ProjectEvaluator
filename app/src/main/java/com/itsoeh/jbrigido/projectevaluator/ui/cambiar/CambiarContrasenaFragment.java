f1c6504fd5df08d9be3dc9abdba53b39d009a581
BR1G1DO <jon5644568@gmail.com>
2024-09-04 17:17:07 -0600

    Se moviò carpeta de cambair a ui

--- app/src/main/java/com/itsoeh/jbrigido/projectevaluator/ui/cambiar/CambiarContrasenaFragment.java
+++ app/src/main/java/com/itsoeh/jbrigido/projectevaluator/ui/cambiar/CambiarContrasenaFragment.java
@@ -1,4 +1,4 @@
-package com.itsoeh.jbrigido.projectevaluator.cambiar;
+package com.itsoeh.jbrigido.projectevaluator.ui.cambiar;
 
 import android.os.Bundle;
 
@@ -6,13 +6,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
